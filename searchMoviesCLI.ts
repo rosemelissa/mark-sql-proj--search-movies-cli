5d4b9de3fb2d54c1e6ebd2166d3d2a3688fbae7c
Melissa Rose <123melissa321@gmail.com>
2022-08-22 17:44:10 +0100

    Version 2: Keep track of favourite movies

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -6,22 +6,62 @@ import { Client } from "pg";
 //we only need to specify the (non-default) database name.
 async function searchMovies() {
 
-    const client = new Client({ database: 'omdb' });
+    
+    let quit = false;
     console.log("Welcome to search-movies-cli!");
-    let searchTerm = question("Search for what movie? (or 'q' to quit): ");
-    let text = 'SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE name ILIKE $1 AND kind=$2 ORDER BY date DESC LIMIT 10';
-    let values = [`%${searchTerm}%`, 'movie']
-    client.connect();
 
-    while (!(searchTerm === 'q')) {
-        try {
-            const res = await client.query(text, values);
-            console.table(res.rows)
-        } catch(err) {
-            console.log(err.stack)
+    while (!quit) {
+        const client = new Client({ database: 'omdb' });
+        client.connect();
+        console.log(`
+        [1] Search
+        [2] See Favourites
+        [3] Quit`);
+        let action = question("Choose an action! [1, 2, 3]: ");
+
+        if (action === '1') {
+            let searchTerm = question("Search term: ")
+            let text = 'SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE name ILIKE $1 AND kind=$2 ORDER BY date DESC LIMIT 10';
+            let values = [`%${searchTerm}%`, 'movie'];
+            try {
+                const res = await client.query(text, values);
+                console.table(res.rows);
+                for (let i = 0; i < res.rows.length; i++) {
+                    console.log(`[${i}] ${res.rows[i].name}`);
+                }
+                let favSelection = question(`Choose a movie row number to favourite [0...${res.rows.length - 1} / q]: `);
+                if ((typeof parseInt(favSelection) === 'number') && (parseInt(favSelection) < res.rows.length)) {
+                    let favIndex = parseInt(favSelection);
+                    let favMovie = res.rows[favIndex]
+                    try {
+                        let favText = 'INSERT INTO favourites (movie_id) VALUES($1)';
+                        let favValues = [favMovie.id];
+                        console.log(`Saving favourite movie: ${favMovie.name}`);
+                        await client.query(favText, favValues);
+                    } catch(err) {
+                        console.log(err.stack);
+                        console.log("Movie could not be saved to favourites");
+                    }
+                } else if (!(favSelection === 'q')) {
+                    console.log("Invalid selection");
+                }
+                
+            } catch(err) {
+                console.log(err.stack)
+            }
+        } else if (action === '2') {
+            console.log("Here are your saved favourites!");
+            let text = 'SELECT movies.id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies JOIN favourites ON movies.id=favourites.movie_id ORDER BY date DESC LIMIT 10';
+            try {
+                const res = await client.query(text);
+                console.table(res.rows)
+            } catch(err) {
+                console.log(err.stack)
+            }
+        } else if (action === '3') {
+            quit = true;
         }
-        searchTerm = question("Search for what movie? (or 'q' to quit): ");
-        values = [`%${searchTerm}%`, 'movie']
-    } await client.end();
+        await client.end();
+    } 
 }
 searchMovies();
