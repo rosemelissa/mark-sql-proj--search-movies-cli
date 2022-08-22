9c52dbeb28e11ab25b6ea3b60d92777eab0e8a51
Melissa Rose <123melissa321@gmail.com>
2022-08-22 13:35:58 +0100

    Version 1: User can search for movie

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,24 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+async function searchMovies() {
+
+    const client = new Client({ database: 'omdb' });
+    console.log("Welcome to search-movies-cli!");
+    let searchTerm = question("Search for what movie? (or 'q' to quit): ");
+    let text = 'SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE name ILIKE $1 AND kind=$2 ORDER BY date DESC LIMIT 10';
+    let values = [`%${searchTerm}%`, 'movie']
+    client.connect();
+
+    while (!(searchTerm === 'q')) {
+        try {
+            const res = await client.query(text, values);
+            console.table(res.rows)
+        } catch(err) {
+            console.log(err.stack)
+        }
+        searchTerm = question("Search for what movie? (or 'q' to quit): ");
+        values = [`%${searchTerm}%`, 'movie']
+    } await client.end();
+}
+searchMovies();
